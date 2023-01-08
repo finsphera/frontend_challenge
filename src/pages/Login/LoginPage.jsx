a8400b6d160cde4bbe056800bab2cbed5e67156f
Joaquin Angeles <jqn.ngls@gmail.com>
2023-01-07 19:15:10 -0700

    Created routing paths v1

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+const Loginpage = () => {
+	return <div className="app">Login</div>;
+};
+
+export default Loginpage;
---
+++ src/pages/Register/RegisterPage.jsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+	return <div className="app">Register</div>;
+};
+
+export default RegisterPage;
