0ead658e38c981f57354bf4063b167d099c202d0
Joaquin Angeles <jqn.ngls@gmail.com>
2023-01-09 16:30:30 -0700

    Logo component and design

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import logo1 from "../../assets/your-logo-here.png";
+import "./logo.css";
+
+const Logo = () => {
+	return (
+		<div>
+			<img src={logo1} alt="logo" />
+		</div>
+	);
+};
+
+export default Logo;
