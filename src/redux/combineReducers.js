662e70d3a3008cff3aeb770c0b1de385f3a155cc
Joaquin Angeles <jqn.ngls@gmail.com>
2023-01-12 22:09:16 -0700

    Decided to save characters on redux store

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./users/reducer";
+import characterReducer from "./characters/reducer";
 
 export const reducer = combineReducers({
 	userReducer,
+	characterReducer,
 });
