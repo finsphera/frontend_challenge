da23aa24383a3d9f4f9155aefb7b6bc749a58c0f
Joaquin Angeles <jqn.ngls@gmail.com>
2023-01-12 18:57:14 -0700

    Make api call to get characters

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,12 +1,68 @@
+import { faFaceLaughBeam, faQuestion, faSkull } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./characters.css";
 
 const Characters = () => {
+	const [charList, setCharlist] = useState([]);
+
+	const generateNumArray = () => {
+		//const randomNum = Math.floor(Math.random() * 826);
+		const randomArray = Array(8)
+			.fill()
+			.map(() => Math.floor(1 + (826 - 1 + 1) * Math.random()));
+
+		console.log("a random number", randomArray);
+
+		return randomArray;
+	};
+
+	const getRandomCharacters = useCallback(() => {
+		//Usecallback es para retener la ref de la funcion 1 sola vez...
+		axios
+			.get(`https://rickandmortyapi.com/api/character/${generateNumArray()}`)
+			.then((res) => {
+				console.log(res.data);
+				setCharlist(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [setCharlist]);
+
+	useEffect(() => {
+		getRandomCharacters();
+	}, [getRandomCharacters]);
+
 	return (
 		<section className="cha-container">
-			<h1>Characters section</h1>
-			<button onClick={getRandomChar}></button>
+			<div className="character-holder">
+				{charList.length > 0 &&
+					charList.map((person) => {
+						return (
+							<div className="card" key={person.id}>
+								<img src={person.image} alt="a character" className="card-img" />
+								<div className="card-details">
+									<div className="card-status-title">
+										{person.status === "Alive" ? (
+											<FontAwesomeIcon icon={faFaceLaughBeam} className="fa-2x left-icon" color="#28a745" />
+										) : person.status === "Dead" ? (
+											<FontAwesomeIcon icon={faSkull} className="fa-2x left-icon" color="#dc3545" />
+										) : (
+											<FontAwesomeIcon icon={faQuestion} className="fa-2x left-icon" color="#6c757d" />
+										)}
+										<h3 className="card-title">{person.name}</h3>
+									</div>
+									<label className="card-label-1">Species: {person.species}</label>
+									<label className="card-label-2">Gender: {person.gender}</label>
+									<label className="card-label-3">Origin: {person.origin.name}</label>
+								</div>
+							</div>
+						);
+					})}
+			</div>
+			<button onClick={getRandomCharacters}>Get Random Characters!</button>
 		</section>
 	);
 };
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,18 +1,16 @@
-import { useSelector } from "react-redux";
 import Characters from "../../components/Characters/Characters";
+import HeroHeader from "../../components/HeroHeader/HeroHeader";
 import Searchbar from "../../components/Searchbar/Searchbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import "./homepage.css";
 
 const HomePage = () => {
-	const user = useSelector((state) => state.userReducer.user);
 	return (
 		<main className="main-container">
 			<Sidebar />
 			<div className="flex-right">
 				<Searchbar />
-				<h1>Home Page</h1>
-				<div>Welcome back {user.username}!</div>
+				<HeroHeader />
 				<Characters />
 			</div>
 		</main>
