99ed911fb88fdba0a3c930f57d556c7b5772adca
Josom0 <jorge.solis@homie.mx>
2024-05-28 15:16:43 -0600

    fix: errors

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { IMovieData } from "@/types/Movies"
+import { IResponseData } from "@/types/Request"
 
 export const checkStatus = (resp: any) => {
   if (resp.status >= 200 && resp.status < 300) {
@@ -8,6 +8,6 @@ export const checkStatus = (resp: any) => {
   }
 }
 
-export const Random = (array: IMovieData[]) => {
+export const Random = (array: IResponseData[]) => {
   return array[Math.floor(Math.random() * array.length - 1)]
 }
